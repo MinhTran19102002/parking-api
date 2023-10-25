0a984ea125b2637395cf8b32378a3ba0413ed516
MinhTran19102002 <115199643+MinhTran19102002@users.noreply.github.com>
2023-10-25 22:50:06 +0700

    authentication

--- src/middlewares/verifytokenMidleware.js
+++ src/middlewares/verifytokenMidleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
-import {JWT_ACCESS_KEY} from '~/config/environment'
+import { env } from '~/config/environment'
+import { StatusCodes } from 'http-status-codes'
 
 const createToken = (User, Secret, Tokenlife) => {
   const token = jwt.sign(User, Secret, {
@@ -13,7 +14,16 @@ const verifyToken = (req, res, next) => {
   const token = req.header.token
   if (token) {
     const accessToken = token.split(' ')[1]
-    jwt.verify(accessToken, JWT_ACCESS_KEY)
+    jwt.verify(accessToken, env.JWT_ACCESS_KEY, (err, user) =>{
+      if (err) {
+        res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token is not valid' })
+      }
+      req.user = user
+      next()
+    })
+  }
+  else {
+    res.status(StatusCodes.FORBIDDEN).json({message: 'You are not authenticated'})
   }
 }
 
